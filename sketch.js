94f86fc7fb38f37fd0e6aa980ec6f5453325498a
KeerthyKarthikeyan <70695834+KeerthyKarthikeyan@users.noreply.github.com>
2020-09-29 17:50:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+const World= Matter.World;
+const Engine= Matter.Engine;
+const Bodies= Matter.Bodies;
+
+var engine, world;
+//var ground, ball;*/
+var box1, box2, ground, pig1,log1;
+
+function setup() {
+  createCanvas(1200,400);
+  engine = Engine.create();
+world= engine.world;
+
+ground= new Ground(600,height,1200,20)
+
+
+box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    pig1 = new Pig(810, 350);
+    log1 = new Log(810,260,300, PI/2);
+
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    pig3 = new Pig(810, 220);
+
+    log3 =  new Log(810,180,300, PI/2);
+}
+
+function draw() {
+  background(0); 
+  Engine.update(engine)
+  /*rectMode(CENTER)
+  rect(ground.position.x,ground.position.y, 400, 30); 
+  rectMode(RADIUS);
+ // ellipse(ball.position.x,ball.position.y,20,20)*/
+  box1.display();
+  box2.display();
+  ground.display();
+  pig1.display()
+  log1.display();
+
+
+  box3.display();
+box4.display();
+pig3.display();
+log3.display();
+
+  drawSprites();
+}
